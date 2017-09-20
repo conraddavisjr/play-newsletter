06b44df413a1d27d32105a14cfb6f0b3c2a9fba1
Conrad Davis Jr <conraddavis@google.com>
2017-09-20 09:32:33 -0700

    looped through the newsletter top 5, per the JSON data

--- src/play-newsletter/template.js
+++ src/play-newsletter/template.js
@@ -4,24 +4,27 @@
 
 */
 
-
-import newsletterData from './data/newsletter-data.json'; 
-
-console.log('newsletterData: ', newsletterData.topFive.games )
-
-const title = 'TOP NEW DOWNLOADED GAMES'
-const bgColor = '#f4f4f4'
-
 // container for the template's output
 let templateOutput = '';
 // identify the newsletter element as the output container
 const newsletter = document.getElementById('newsletter');
+
+import newsletterData from './data/newsletter-data.json'; 
+const { topFive } = newsletterData
+
+console.log('topFive: ', Object.keys(topFive))
+
+
+console.log('newsletterData: ', newsletterData.topFive.games )
+
+const bgColor = '#f4f4f4'
 
 // import the newsletter modules
 import docHead from './modules/head.js';
 import header from './modules/header.js';
 import introBody from './modules/introBody.js';
 import trendingApps from './modules/trendingApps.js';
+import topFiveItems from './helpers/topFiveItems.js';
 import topGames from './helpers/topFiveItems.js';
 import topSongs from './helpers/topFiveItems.js';
 
@@ -32,16 +35,17 @@ templateOutput += docHead
 templateOutput += header
 // add the newsletter's intro body to the templateOutput
 templateOutput += introBody
-// 
+// add the trending apps section to the templateOutput
 templateOutput += trendingApps
-// title, imgWidth, imgHeight, rowItems
 
+// 
+// TOP FIVE SECTION
+// 
 templateOutput += `<table class="top_five_row" role="presentation" align="center" cellpadding="0" cellspacing="0" border="0" width="100%">`
 
-templateOutput += topGames(newsletterData.topFive.games.title, newsletterData.topFive.games.rowItems)
-// 
-templateOutput += topSongs(newsletterData.topFive.games.title, newsletterData.topFive.games.rowItems)
-templateOutput += topSongs(newsletterData.topFive.games.title, newsletterData.topFive.games.rowItems)
+Object.keys(topFive).map((rowItem) => {
+  templateOutput += topFiveItems(topFive[rowItem].title, topFive[rowItem].rowItems)
+})
 
 templateOutput += `</table>`
 
